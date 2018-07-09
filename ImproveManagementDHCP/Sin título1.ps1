6fe9c66a052bd00b7eb4a3443758965d8db495b4
Ricard Comas <amateo@xaloc.org>
2018-07-09 19:00:59 +0200

    Hello

---
+++ ImproveManagementDHCP/Sin título1.ps1
@@ -0,0 +1,4 @@
+﻿$string = $MyInvocation.InvocationName
+$remove = $string.Split("\")
+$path = $string.Replace($remove.Get($remove.Length-1),"")
+echo $path"Hello"
---
+++ More/Test-ConnectionIPRange.psm1
@@ -0,0 +1,16 @@
+﻿function Test-ConnectionIPRange
+{
+  param (
+    [parameter(Mandatory=$true)][IPAddress]$firstIP,
+    [parameter(Mandatory=$true)][IPAddress]$lastIP
+  )
+  
+  $lastIndex = [Convert]::ToString($firstIP.IPAddressToString,2)
+  write $lastIndex
+  for($i = $firstIP;$i -le $lastIP;$i++)
+  {
+    if(!(Test-Connection -ComputerName $i -Quiet)){
+      
+    }
+  }
+}
